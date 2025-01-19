d279c94bfa726c3cc3f446857460da35d8baf90b
ConniConni <nmtgktmo@gmail.com>
2025-01-19 18:48:17 +0900

    ボタン追加

--- game_view.py
+++ game_view.py
@@ -1,4 +1,4 @@
-from tkinter import Canvas
+from tkinter import Canvas, Button
 
 CELL_SIZE = 30
 
@@ -8,3 +8,10 @@ class GameView:
 
         # キャンバス
         self.canvas = Canvas(root, width=model.width * CELL_SIZE, height=model.height * CELL_SIZE, bg="black")
+        self.canvas.pack(side="left")
+
+        # ボタン
+        self.start_button = Button(root, text="Start")
+        self.canvas.pack(side="top")
+        self.reset_button = Button(root, text="Reset")
+        self.reset_button.pack(side="top")
