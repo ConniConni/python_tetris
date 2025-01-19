c81c2a52cc0fa4ccbfb874ccd45872007f5dc823
ConniConni <nmtgktmo@gmail.com>
2025-01-19 17:14:51 +0900

    ゲームモデル、ビュー、コントローラを初期化

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from tkinter import Tk
 from game_model import GameModel
+from game_view import GameView
+from game_controller import GameController
 
 def main():
     # Tkinter ウィンドウを作成
     root = Tk()
     root.title("Tetris")
     print("make window")
+
+    # ゲームモデル、ビュー、コントローラを初期化
+    model = GameModel()
+    view = GameView(root,model)
+    controller = GameController(model ,view)
 
 if __name__ == "__main__":
     main()
